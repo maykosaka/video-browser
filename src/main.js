4d03fed0eb5ba56c67e3208742a92254d1fad70e
May K <may.kosaka@gmail.com>
2018-08-02 16:44:06 +0900

    Render to screen

--- src/main.js
+++ src/main.js
@@ -1,1 +1,6 @@
 import Vue from 'vue';
+import App from './App';
+
+new Vue({
+    render: h => h(App) // refactor of function(h) {return h(App)};
+}).$mount('#app'); // same as element: where to render the element html
