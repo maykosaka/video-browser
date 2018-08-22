181e2095f4a4e9bfd0c432b9f1940bd7aef03587
May K <may.kosaka@gmail.com>
2018-08-22 15:31:24 +0900

    Render video list to screen

--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -1,13 +1,25 @@
 <template>
     <ul>
-        VideoList
+        <VideoListItem
+            v-for="video in videosFound"
+            :video="video"
+            :key="video.etag"
+        >
+        </VideoListItem>
     </ul>
 </template>
 
 <script>
+import VideoListItem from './VideoListItem';
+
 export default {
     name: 'VideoList',
-    props: ['videosFound']
+    components: {
+        VideoListItem
+    },
+    props: {
+        videosFound: Array // prop validation for type of prop
+    }
 };
 </script>
 
